1eeae90b9548791d1ea823879bb0d271e69e313b
Pavel Devitskiy <devitskiy@gmail.com>
2024-12-03 08:20:35 +0700

    Update

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf('7277567974:AAGKLsI0I4xeDrIijiDBaH0obTjwjGuB_h0');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,6 +15,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+
+
+
+bot.command('signup', (ctx) => {
+  ctx.reply('Откройте мини-приложение для записи!', {
+    reply_markup: {
+      inline_keyboard: [[{ text: 'Записаться на игру', web_app: { url: 'https://your-app.vercel.app/' } }]],
+    },
+  });
+});
+
+bot.launch();
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
